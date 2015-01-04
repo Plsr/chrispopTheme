2fba4070981438abeb54312f9fbd710c9e242739
plsr <christianpoplawski@gmail.com>
2015-01-04 16:18:18 +0100

    Final content for author info

--- author-info.php
+++ author-info.php
@@ -1,13 +1,13 @@
 <section class="author-info ">
 	<div class="author-data clearfix">
-	<img src="<?php echo get_template_directory_uri(); ?>/portrait.png">
+	<img src="<?php echo get_template_directory_uri(); ?>/img/portrait.png">
 	<div class="author-text">
 		<h3>Hi, I'm Chris</h3>
-		<p>Nostrud short ribs tail flank eu pig. Consectetur cillum ut nostrud. Cupim swine labore beef ribs kielbasa duis. Pariatur labore sunt, swine est jerky do. </p>
+		<p>I study Media and Informatics in Cologne, Germany. This Weblog is a loose collection of things I find interesting.</p>
 	</div>
 	</div>
 	<div class="connect">
-		<a class="icon-twitter" href="http://twitter.com"> &#xe601;</a>
-		<a class="icon-paperplane" href="mailto:mail@christianpoplawski.de"> &#xe600;</a>
+		<a class="icon-twitter" href="http://twitter.com/_chrispop"> &#xe601;</a>
+		<a class="icon-paperplane" href="mailto:hi@chrispop.de"> &#xe600;</a>
 	</div>
 </section>
