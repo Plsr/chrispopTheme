aab54e9f7dfbf20b3e120da946c6c03a8605714a
plsr <christianpoplawski@gmail.com>
2016-06-04 20:56:32 +0200

    Squashed commit of the following:
    commit 24964dc1caaf91ddfbdb4d45f55f4112fe8287b5
    Author: plsr <christianpoplawski@gmail.com>
    Date:   Sat Jun 4 20:35:48 2016 +0200
    
        Added Pagination to Link List
    
        Also imprved structur of the file
    
    commit 03832aa0e75b380508233ae7ed304f84ae9db3e5
    Author: plsr <christianpoplawski@gmail.com>
    Date:   Fri Oct 9 23:31:57 2015 +0200
    
        Quickfix, permalinks not working
    
        Too tired to look into this now, this is a somewhat dirty workaround
    
    commit d1ca2a0821d4b034b1670ba4a016a58675e38c4e
    Merge: e9db8ff 6494244
    Author: plsr <christianpoplawski@gmail.com>
    Date:   Fri Oct 9 22:43:46 2015 +0200
    
        Merge branch 'feature/LinkedList' into develop
    
    commit 64942448d9d77710e1950d7e23cd558a3ee350cd
    Author: plsr <christianpoplawski@gmail.com>
    Date:   Fri Oct 9 22:42:53 2015 +0200
    
        Minor style tweaks for link list description
    
        Added the .-dark class. Only used in link list description
    
    commit cf6da2bfbb73673213a6ed0800d9664bb9686b0d
    Author: plsr <christianpoplawski@gmail.com>
    Date:   Fri Oct 9 22:42:09 2015 +0200
    
        Added Page for daily links
    
        In here, we have a title and short description of this section as well
        as the query cathing all the link posts.
    
    commit 4179e4cc00a8253b3e9326600fe9e1c9778acf8c
    Author: plsr <christianpoplawski@gmail.com>
    Date:   Fri Oct 9 22:40:51 2015 +0200
    
        Added custom post type for daily link
    
        This way, it's easier to have a loop with just link posts and keep them
        away from the regualr loop.
    
        Also, feels a lot more like hard coupling than using tags or categories.

---
+++ linkedlist.php
@@ -0,0 +1,47 @@
+<?php /* Template Name: Link List */ ?>
+<?php get_header(); ?>
+
+<h2 class="page-title"><?php the_title(); ?></h2>
+<?php if (have_posts()): ?> 
+	<?php  while (have_posts()) : the_post(); ?>
+		<div class="post-content -dark" id="post-<?php the_ID(); ?>">
+			<?php the_content(); ?>
+		</div>
+	<?php endwhile; ?>
+
+<?php else: ?>
+	<article>
+		<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+	</article>
+<?php endif; ?>
+
+<?php $query = new WP_Query( array( 'post_type' => 'daily_link', 'posts_per_page' => '1')); ?>
+	<?php while ( $query->have_posts() ) { $query->the_post(); ?>
+		<article>
+			<h2><?php the_title(); ?></h2>
+			<?php if(is_single()): ?>
+				<span class="single-date"><?php the_time('F j, Y'); ?></span>
+			<?php endif; ?>
+
+			<div class="post-content">
+				<?php the_content('Read more...'); ?>
+			</div>
+
+			<div class="meta">
+				<span class="date"> written <?php the_time('F j, Y'); ?></span>
+			</div>
+
+		</article>
+		<div class="pagination clearfix">	
+			<div class="nav-next"><?php previous_posts_link( 'Newer Links', $query->max_num_pages ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( 'Older Links', $query->max_num_pages ); ?></div>
+		</div>
+	<?php } wp_reset_postdata(); ?>
+
+
+				<div class="nav-next alignright"><?php previous_posts_link( 'Newer posts', $query->max_num_pages ); ?></div>
+
+
+
+
+<?php get_footer(); ?>
