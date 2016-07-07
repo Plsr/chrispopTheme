5dca7209fb6fa84fca184a3c65d1c16225f7d954
plsr <christianpoplawski@gmail.com>
2016-07-07 16:25:01 +0200

    Added production mode to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
   return gulp.src('styles/*.scss')
     .pipe(sass())
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('production', function() {
+  return gulp.src('styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(''));
 });
 
