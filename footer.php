ac7a217355b76314e6109571c3c0024b0b10f81c
Christian Poplawski <christianpoplawski@gmail.com>
2016-07-08 12:35:26 +0200

    Optimizing for Google Page Speed (#27)
    * Removed Google Fonts Links as they are not used.
    
    Closes #21
    
    * Added node_modules to .gitignore
    
    * Added production mode to gulp
    
    * Removed node_modules which should be ignored
    
    * Now checking if wordpress is running in localhost
    
    If so, the regualr css is loaded. Otherwise, wordpress loads the
    minified version.
    Also changed the gulpfile.js accordingly.
    
    See #25
    
    * Removed unused js scripts and minified remaining
    
    The gulpfile now has a function to minify js. The production function is
    calling 'minify-css' and 'minify-js' now instead of doing it directly.
    
    In the footer, the minified js script is only included if worpdress is
    not currently running on localhost.
    
    close #23

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-			</div><!-- /wrapper --> 
+			</div><!-- /wrapper -->
 			<footer>
 				<p>
-					&copy; <?php echo date('Y'); ?> Copyright <a href="http://twitter.com/_chrispop">&#64;_chrispop</a>. 
+					&copy; <?php echo date('Y'); ?> Copyright <a href="http://twitter.com/_chrispop">&#64;_chrispop</a>.
 					<br> Powered by
 					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
 				</p>
@@ -9,12 +9,20 @@
 		</div><!-- /wrapper -->
 		<?php wp_footer(); ?>
 
+
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fitvids.js"></script>
-		
+
+    <?php if (substr($_SERVER['REMOTE_ADDR'], 0, 4) == '127.' || $_SERVER['REMOTE_ADDR'] == '::1'): ?>
+      <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fitvids.js"></script>
+    <?php else: ?>
+      <script src="<?php echo get_template_directory_uri(); ?>/js/all.min.js"></script>
+    <?php endif ?>
+
+
+
 	</body>
-	
-	
+
+
 	<script>
   		$(document).ready(function(){
     		// Target your .container, .wrapper, .post, etc.
--- header.php
+++ header.php
@@ -3,31 +3,34 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 
+    <!-- Check if Wordpress is running on localhost. If so, load the uglyfied css -->
+    <?php if (substr($_SERVER['REMOTE_ADDR'], 0, 4) == '127.' || $_SERVER['REMOTE_ADDR'] == '::1'): ?>
+      <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" >
+    <?php else: ?>
+      <link href="<?php echo get_template_directory_uri(); ?>/style.min.css" rel="stylesheet" type="text/css" >
+    <?php endif ?>
+
 		<!-- Meta tags -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-		<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" >
-		
-		
+
+
+
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
     <link href="<?php echo get_template_directory_uri(); ?>/img/logo.png" rel="shortcut icon">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-    <!-- Webfonts -->
-    <link href='http://fonts.googleapis.com/css?family=Cabin:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
-		
 		<!-- Typekit -->
 		<script src="//use.typekit.net/oqa5pak.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
-    
+
     <?php wp_head(); ?>
 </head>
 
 <body>
 	<!-- header -->
-	<header class="header-bg clearfix">			
+	<header class="header-bg clearfix">
 		<div class="header-content">
 			<ul class="branding">
 				<li class="logo">
@@ -37,11 +40,11 @@
 					<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 				</li>
 			</ul>
-	
+
 			<ul class="nav">
 				 <?php wp_list_pages('title_li='); ?>
 			</ul>
-		
+
 		</div>
 	</header>
 
