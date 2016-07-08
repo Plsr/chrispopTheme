c5ec2d9d381300db7e82297476e3707b00c8ba5a
plsr <christianpoplawski@gmail.com>
2016-07-08 11:53:12 +0200

    Removed unused js scripts and minified remaining
    The gulpfile now has a function to minify js. The production function is
    calling 'minify-css' and 'minify-js' now instead of doing it directly.
    
    In the footer, the minified js script is only included if worpdress is
    not currently running on localhost.
    
    close #23

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+var gulp   = require('gulp'),
+    sass   = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
+// Compile SASS Files to regualr css
 gulp.task('sass', function() {
   return gulp.src('styles/*.scss')
     .pipe(sass())
     .pipe(gulp.dest(''));
 });
 
-gulp.task('production', function() {
+// Minify CSS files
+gulp.task('minify-css', function() {
   return gulp.src('styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest(''));
 });
 
+// Minify JS Files
+gulp.task('minify-js', function() {
+  return gulp.src(['js/*.js', '!js/*.min.js'])
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+// Minify files for production
+// TODO: Include autoprefixer
+gulp.task('production', ['minify-js', 'minify-css'], function() {
+
+});
+
+// Watch task for development
+// TODO: Error logs
 gulp.task('watch', function() {
   gulp.watch('styles/*.scss', ['sass']);
 });
