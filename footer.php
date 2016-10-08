b00ddd73f98b2f10be7fdfcfbc4d9ba87e68cb28
plsr <christianpoplawski@gmail.com>
2016-10-08 20:46:52 +0200

    Some more simplifications

--- footer.php
+++ footer.php
@@ -1,35 +1,11 @@
 			</div><!-- /wrapper -->
 			<footer>
 				<p>
-					&copy; <?php echo date('Y'); ?> Copyright <a href="http://twitter.com/_chrispop">&#64;_chrispop</a>.
-					<br> Powered by
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+					&copy; <?php echo date('Y'); ?> Copyright <a href="http://twitter.com/_chrispop">&#64;_chrispop</a>
 				</p>
+        <a href="impressum">Impressum</a>
 			</footer>
 		</div><!-- /wrapper -->
 		<?php wp_footer(); ?>
-
-
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-    <?php if (substr($_SERVER['REMOTE_ADDR'], 0, 4) == '127.' || $_SERVER['REMOTE_ADDR'] == '::1'): ?>
-      <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fitvids.js"></script>
-    <?php else: ?>
-      <script src="<?php echo get_template_directory_uri(); ?>/js/all.min.js"></script>
-    <?php endif ?>
-
-
-
 	</body>
-
-
-	<script>
-  		$(document).ready(function(){
-    		// Target your .container, .wrapper, .post, etc.
-    		$(".post-content").fitVids({
-    			customSelector: "iframe[src^='http://youtube.googleapis.com']"
-    		});
-  		});
-		</script>
-
 </html>
