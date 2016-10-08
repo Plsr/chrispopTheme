7f192649a1a01694d067fd1331f6bc89fcc849f5
plsr <christianpoplawski@gmail.com>
2016-10-08 18:25:17 +0200

    Added css injection
    Also fixed some typos and made gulp watch subfolders in styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
-    brwoserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create();
 
 // Compile SASS Files to regualr css
 gulp.task('sass', function() {
-  gulp.src('styles/*.scss')
+  gulp.src('styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest(''))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('serve', ['sass'], function() {
-  brwoserSync.init({
+  browserSync.init({
     proxy: "http://localhost:8888/wordpress/"
   });
 
-  gulp.watch('styles/*.scss', ['sass']);
+  gulp.watch('styles/**/*.scss', ['sass']);
 })
