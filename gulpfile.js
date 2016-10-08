41848726b23426a0ce596563d5861c841d452433
plsr <christianpoplawski@gmail.com>
2016-10-08 18:13:24 +0200

    Updated Build Process
    Removed all minification for now and added brwoserSync to gulpfile.
    Also switched from scss to sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,18 @@
-var gulp   = require('gulp'),
-    sass   = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    brwoserSync = require('browser-sync').create();
 
 // Compile SASS Files to regualr css
 gulp.task('sass', function() {
-  return gulp.src('styles/*.scss')
-    .pipe(sass())
+  gulp.src('styles/*.sass')
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(''));
 });
 
-// Minify CSS files
-gulp.task('minify-css', function() {
-  return gulp.src('styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest(''));
-});
+gulp.task('serve', ['sass'], function() {
+  brwoserSync.init({
+    proxy: "http://localhost:8888/wordpress/"
+  });
 
-// Minify JS Files
-gulp.task('minify-js', function() {
-  return gulp.src(['js/*.js', '!js/*.min.js'])
-    .pipe(concat('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'));
-});
-
-// Minify files for production
-// TODO: Include autoprefixer
-gulp.task('production', ['minify-js', 'minify-css'], function() {
-
-});
-
-// Watch task for development
-// TODO: Error logs
-gulp.task('watch', function() {
-  gulp.watch('styles/*.scss', ['sass']);
-});
+  gulp.watch('styles/*.sass', ['sass']);
+})
